a025ad4b85212e538edc3fce0513569ba4ca6700
ArjnCrly <arjuna.carly1304@gmail.com>
2024-02-22 18:56:49 +0700

    Create Buku04.java

---
+++ Pertemuan 2/Buku04.java
@@ -0,0 +1,27 @@
+/**
+ * Buku04
+ */
+public class Buku04 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: Rp");
+    }
+    void terjual (int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } 
+    }
+    void restock (int jml) {
+        stok += jml;
+    }
+    void gantiHarga (int hrg) {
+        harga = hrg;
+    }
+}
