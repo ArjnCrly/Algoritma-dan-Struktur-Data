d1b5bda3f07c4cbaf7c0429215f57a06fad9d30c
ArjnCrly <arjuna.carly13042gmail.com>
2024-02-23 15:42:11 +0700

    feat: add new method

--- Pertemuan2/Buku04.java
+++ Pertemuan2/Buku04.java
@@ -8,22 +8,29 @@ public class Buku04 {
     Buku04() {
         
     }
-    Buku04(String jud, String pg, int hal, int stok, int har) {
+    Buku04(String jud, String pg, int hal, int stok, int har,int jml) {
         judul = jud;
         pengarang = pg;
         halaman = hal;
         this.stok = stok;
         harga = har;
+        jumlah = jml;
+
     }
     String judul, pengarang;
-    int halaman, stok, harga;
+    int halaman, stok, harga, jumlah, hargaTotal, diskon;
 
     void tampilInformasi() {
+        System.out.println("-----------------------------");
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
         System.out.println("Jumlah Halaman: " + halaman);
         System.out.println("Sisa Stok: " + stok);
-        System.out.println("Harga: Rp" + harga);
+        System.out.println("Harga: Rp " + harga);
+        System.out.println("Harga Total: Rp " + hitungHargaTotal());
+        System.out.println("Diskon: Rp " + hitungDiskon());
+        System.out.println("Jumlah yang dibeli: " + jumlah);
+        System.out.println("Harga yang dibayar: Rp " + hitungHargaBayar());
     }
     void terjual (int jml) {
         if (stok > 0) {
@@ -36,4 +43,20 @@ public class Buku04 {
     void gantiHarga (int hrg) {
         harga = hrg;
     }
-}
+    int hitungHargaTotal () {
+        return harga * jumlah;
+    }
+    int hitungDiskon () {
+        if (hitungHargaTotal() > 150000) {
+            diskon = (int)(0.12 * hitungHargaTotal());
+        } else if (hitungHargaTotal()>75000) {
+            diskon = (int)(0.05 * hitungHargaTotal());
+        } else {
+            diskon = 0;
+        } 
+        return diskon;
+    }
+    int hitungHargaBayar () {
+        return harga * jumlah - hitungDiskon();
+    }
+}    
--- Pertemuan2/BukuMain04.java
+++ Pertemuan2/BukuMain04.java
@@ -3,23 +3,23 @@ package Pertemuan2;
     public class BukuMain04 {
 
         public static void main(String[] args) {
-            Buku04 bk1 = new Buku04();
-            bk1.judul = "Today Ends Tomorrow Comes";
-            bk1.pengarang = "Denandra Pratiwi";
-            bk1.halaman = 198;
-            bk1.stok = 13;
-            bk1.harga = 71000;
+            // Buku04 bk1 = new Buku04();
+            // bk1.judul = "Today Ends Tomorrow Comes";
+            // bk1.pengarang = "Denandra Pratiwi";
+            // bk1.halaman = 198;
+            // bk1.stok = 13;
+            // bk1.harga = 71000;
 
-            bk1.tampilInformasi();
-            bk1.terjual(5);
-            bk1.gantiHarga(60000);
-            bk1.tampilInformasi();
+            // bk1.tampilInformasi();
+            // bk1.terjual(5);
+            // bk1.gantiHarga(60000);
+            // bk1.tampilInformasi();
 
-            Buku04 bk2 = new Buku04("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+            Buku04 bk2 = new Buku04("Self Reward", "Maheera Ayesha", 160, 29, 59000, 2);
             bk2.terjual(11);
             bk2.tampilInformasi();
             
-            Buku04 bukuArjuna = new Buku04("Atomic Habits", "James clear", 340, 10, 11000000);
+            Buku04 bukuArjuna = new Buku04("Atomic Habits", "James clear", 340, 10, 11000000, 3);
             bukuArjuna.terjual(1);
             bukuArjuna.tampilInformasi();
         }
